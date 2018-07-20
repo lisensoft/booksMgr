0d4d41008fbf994259c5ca39ad430314baf6899f
lisensoft <sen.li@sihuisoft.com.cn>
2018-07-20 23:03:13 +0800

    Signed-off-by: lisensoft <sen.li@sihuisoft.com.cn>

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import user from './modules/user'
+import getters from './getters'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  state: {
-    userName: 'ghostwu'
-  }
+  modules: {
+    user
+  },
+  getters
 })
 
 export default store
---
+++ src/views/login/index.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+       <divider> {{ $t('Horizontal') }} </divider>
+    <flexbox>
+      <flexbox-item><div class="flex-demo">1</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">2</div></flexbox-item>
+    </flexbox>
+    <br>
+    <flexbox>
+      <flexbox-item><div class="flex-demo">1</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">2</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">3</div></flexbox-item>
+    </flexbox>
+    <br>
+    <flexbox>
+      <flexbox-item><div class="flex-demo">1</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">2</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">3</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">4</div></flexbox-item>
+    </flexbox>
+    <br>
+    <flexbox>
+      <flexbox-item><div class="flex-demo">1</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">2</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">3</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">4</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">5</div></flexbox-item>
+    </flexbox>
+    <br>
+    <flexbox>
+      <flexbox-item><div class="flex-demo">1</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">2</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">3</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">4</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">5</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">6</div></flexbox-item>
+    </flexbox>
+    <br>
+    <divider> {{ $t('Honrizontal with no gutter') }} </divider>
+    <flexbox :gutter="0">
+      <flexbox-item><div class="flex-demo">1</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">2</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">3</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">4</div></flexbox-item>
+    </flexbox>
+    <br>
+    <divider> {{ $t('Vertical') }} </divider>
+    <flexbox orient="vertical">
+      <flexbox-item><div class="flex-demo">1</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">2</div></flexbox-item>
+    </flexbox>
+    <br>
+    <divider> {{ $t('Vertical with no gutter') }} </divider>
+    <flexbox orient="vertical" :gutter="0">
+      <flexbox-item><div class="flex-demo">1</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">2</div></flexbox-item>
+    </flexbox>
+    <br>
+    <divider> {{ $t('Grid support (12 columns)') }} </divider>
+    <flexbox>
+      <flexbox-item :span="4"><div class="flex-demo">1/3</div></flexbox-item>
+      <flexbox-item><div class="flex-demo">2/3</div></flexbox-item>
+    </flexbox> 
+    </div>
+</template>
+<script>
+import { Flexbox, FlexboxItem, Divider } from 'vux'
+
+export default {
+  components: {
+    Flexbox,
+    FlexboxItem,
+    Divider
+  }
+}
+</script>
